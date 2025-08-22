81787cb8249921f183df01e63ee8e6991965fa92
lsv-7 <www.lokeshsaivenkat.com@gmail.com>
2025-08-22 17:29:13 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=143
+let email="abc@gmail.com"
+var pass=123
+accCity="Vij"
+let state;
+
+//accountId=2
+
+email="xyz@gmail.com"
+pass=456
+accCity="Tnl"
+
+console.log(accountId);
+
+/*
+    Prefer not to use var due to issue in scope
+*/
+
+console.table([accountId,email,pass,accCity,state]);
