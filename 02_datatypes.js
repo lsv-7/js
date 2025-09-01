39dbc1eeca7b3c57a4ca3917f58a71cb0c6f81b2
lsv-7 <www.lokeshsaivenkat.com@gmail.com>
2025-09-01 15:50:33 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat js code as newer version
+
+//alert(2+17) // we are using nodejs not browser
+
+//tc39.es mdn js
+
+//null=standalone value
+//undefined
+//symbol=unique
+
+//object
+
+//console.log(typeof ---)
+
