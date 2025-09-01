1af3c2b1bd9088590de53aea103c478c60cb01cd
lsv-7 <www.lokeshsaivenkat.com@gmail.com>
2025-09-01 17:31:10 +0000

    conversionofdatatypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "lokesh"
+
+console.log(typeof(score))
+
+let n=Number(score)
+
+console.log(typeof(n))
+console.log(n)
+
+// "77" => 77
+// "77lsv" => Nan
+
+
+//Boolean
+// 1 => true ; 0 => false
+// "" => false
+// "lokesh" => true
